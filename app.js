e18760d8438881f9426973f71e995af648afc154
tanzeemraza <tanzeemraza243@gmail.com>
2020-06-23 22:20:37 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,104 @@
+// Chapter 21  to 25  
+
+
+// Task 1
+// var firstname = [ "Muhammad","Tanzeem" ]
+// var lastname = ["Raza"]
+// for (var i = 0; i < firstname.length; i ++){
+//     for ( var x = 0; x < lastname.length; x++){
+//         console.log(firstname[i] + " " + lastname [x])
+//     }
+// }
+// Task 3 
+// no
+
+// var num = 35.36
+// var round = parseInt(num)
+// console.log(round)
+
+
+
+
+// Task 5
+// var a = "Pakistani";
+// console.log(a.charAt(3))
+
+
+
+// Task 7 
+// var text = "Pakistan is the Biggest country of Asia and HYDERABAD is the 7th beggest city of pakistan"
+// for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 9) === "HYDERABAD") {
+//      text = text.slice(0, i) + "ISLAMABAD" + text.slice(i + 9);
+//      console.log (text)
+//      }
+//     }
+
+// Task 8
+// var text = "Pakistan is the Biggest country of Asia and HYDERABAD is the 7th beggest city of pakistan"
+// for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 3) === "and") {
+//      text = text.slice(0, i) + "&" + text.slice(i + 3);
+//      console.log (text)
+//      }
+//     }
+
+// Task 9
+// var text = "Pakistan is the Biggest country of Asia and HYDERABAD is the 472 beggest city of pakistan"
+// for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 3) === "472"){
+//      text = text.slice(0, i) + "456" + text.slice(i + 3);
+//      console.log (text)
+//      }
+//     }
+    
+// Task 10
+// var bics = prompt ("enter your bics name")
+// var firstchar =bics.slice (0,1)
+// var otherchar = bics.slice (1)
+// firstchar = firstchar.toUpperCase ()
+// otherchar = otherchar.toLowerCase ()
+// var bics = firstchar + otherchar;
+// console.log(bics)
+
+// Task 16 
+
+// var word = prompt ("enter your word");
+// var check = " ";
+// for(var i = word.length - 1; i >=0; i --){
+//     console.log(word [i])
+// }
+
+// Chapter 25 to 30 
+// Task 1
+// var num = 2.5;
+// var round = Math.round(num)
+// console.log(round)
+
+// var num = 3.5;
+// var round = Math.floor(num)
+// console.log(round)
+
+// var num = 3.5;
+// var round = Math.ceil(num)
+// console.log(round)
+
+// Task 2 
+// var num = -3.5;
+// var round = Math.round(num)
+// console.log(round)
+
+// var num = -2.5;
+// var round = Math.floor(num)
+// console.log(round)
+
+// var num = -2.2;
+// var round = Math.ceil(num)
+// console.log(round)
+
+
+
+
+
+
+
